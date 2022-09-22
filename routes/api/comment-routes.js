0f6ce4ba211b8b48cd05378ee4e52b7f46288a35
dmadon <deanna.madon@gmail.com>
2022-09-21 23:55:26 -0500

    add reply functionality to comments

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
-const {addComment, removeComment} = require('../../controllers/comment-controller');
+const {addComment, removeComment, addReply, removeReply} = require('../../controllers/comment-controller');
 
 // this is the /api/comments/<pizzaId> route
 router.route('/:pizzaId').post(addComment);
 
 // this is the /api/comments/<pizzaId>/<commentId> route
-router.route('/:pizzaId/:commentId').delete(removeComment)
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+// this is the /api/comments/<pizzaId>/<commentId>/<replyId> route
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
 
 
 module.exports = router;
